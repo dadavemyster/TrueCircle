32034212825c08a2d51afade383964827f16ef6d
Wixisiz <SennyLu101@gmail.com>
2025-08-12 16:11:36 -0400

    made light dark mode stay between page changes

--- js/lightdarkmode.js
+++ js/lightdarkmode.js
@@ -1,7 +1,30 @@
-let isLight = true;
+let isLight = null;
 let bodyBackground = document.getElementById("bg");
 let imageButton = document.getElementById("lightdark");
 let textColor = document.getElementsByClassName("lightdarkchange");
+
+if (localStorage.getItem("lightmode") == null) {
+    localStorage.setItem("lightmode","true");
+    isLight = true;
+} else if (localStorage.getItem("lightmode") == "true") {
+    bodyBackground.classList.remove("bg-dark");
+    bodyBackground.classList.add("bg-light");
+    imageButton.src = "img/sun.png";
+    for (let i = 0; i < textColor.length; i++) {
+        textColor[i].classList.add("text-muted");
+        textColor[i].classList.remove("text-white");
+    }
+    isLight = true;
+} else {
+    bodyBackground.classList.add("bg-dark");
+    bodyBackground.classList.remove("bg-light");
+    imageButton.src = "img/moon.png";
+    for (let i = 0; i < textColor.length; i++) {
+        textColor[i].classList.add("text-white");
+        textColor[i].classList.remove("text-muted");
+    }
+    isLight = false;
+}
 
 function lightdark() {
     isLight = !isLight;
@@ -13,6 +36,7 @@ function lightdark() {
             textColor[i].classList.add("text-muted");
             textColor[i].classList.remove("text-white");
         }
+        localStorage.setItem("lightmode","true");
     } else {
         bodyBackground.classList.add("bg-dark");
         bodyBackground.classList.remove("bg-light");
@@ -21,5 +45,6 @@ function lightdark() {
             textColor[i].classList.add("text-white");
             textColor[i].classList.remove("text-muted");
         }
+        localStorage.setItem("lightmode","false");
     }   
 };
